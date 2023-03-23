b60d2cf01e95d80b0befd19914f61f5765a89ec5
ritesh <riteshraj1064@gmail.com>
2023-03-23 17:07:08 +0530

    first2 commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/reset-password" element={<Form />} />
+        <Route path="/" element={<Form />} />
       </Routes>
     </BrowserRouter>
   );
